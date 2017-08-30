08c9cd4787179eaa627eb2036c27b1edc9685517
lphthephunk <pcody8900@gmail.com>
2017-08-30 16:08:03 -0400

    Push for Fix

---
+++ Ameritrack_Xam/Ameritrack_Xam.Android/Pages/CameraPage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+[assembly:ExportRenderer(typeof(Ameritrack_Xam.Pages.Views.CameraPage), typeof(Ameritrack_Xam.Droid.Pages.CameraPage))]
+
+namespace Ameritrack_Xam.Droid.Pages
+{
+    public class CameraPage : PageRenderer, TextureView.ISurfaceTextureListener
+    {
+        global::Android.Hardware.Camera camera;
+        global::Android.Widget.Button takePhotoButton;
+        global::Android.Widget.Button toggleFlashButton;
+        global::Android.Widget.Button toggleCameraButton;
+
+        Activity activity;
+        TextureView textureView;
+        SurfaceTexture surfaceTexture;
+        global::Android.Views.View view;
+
+        bool _isFlashOn;
+
+        byte[] imageData;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+            try
+            {
+                activity = this.Context as Activity;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
