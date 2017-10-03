cec6fcd104c2c7389e2bb7a0652fd6c7cd760d96
Justin Rose <rose.justincode@gmail.com>
2017-10-03 16:00:33 -0400

    Push everything -- need to fix gitignore

--- Ameritrack_Xam/Ameritrack_Xam.Android/obj/Debug/android/src/md5b60ffeb829f638581ab2bb9b1a7f4f3f/LabelRenderer.java
+++ Ameritrack_Xam/Ameritrack_Xam.Android/obj/Debug/android/src/md5b60ffeb829f638581ab2bb9b1a7f4f3f/LabelRenderer.java
@@ -10,6 +10,7 @@ public class LabelRenderer
 	public static final String __md_methods;
 	static {
 		__md_methods = 
+			"n_onTouchEvent:(Landroid/view/MotionEvent;)Z:GetOnTouchEvent_Landroid_view_MotionEvent_Handler\n" +
 			"";
 		mono.android.Runtime.register ("Xamarin.Forms.Platform.Android.LabelRenderer, Xamarin.Forms.Platform.Android, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null", LabelRenderer.class, __md_methods);
 	}
@@ -37,6 +38,14 @@ public class LabelRenderer
 		if (getClass () == LabelRenderer.class)
 			mono.android.TypeManager.Activate ("Xamarin.Forms.Platform.Android.LabelRenderer, Xamarin.Forms.Platform.Android, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
 	}
+
+
+	public boolean onTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_onTouchEvent (p0);
+	}
+
+	private native boolean n_onTouchEvent (android.view.MotionEvent p0);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
--- Ameritrack_Xam/Ameritrack_Xam.Android/obj/Debug/android/src/md5b60ffeb829f638581ab2bb9b1a7f4f3f/Platform_DefaultRenderer.java
+++ Ameritrack_Xam/Ameritrack_Xam.Android/obj/Debug/android/src/md5b60ffeb829f638581ab2bb9b1a7f4f3f/Platform_DefaultRenderer.java
@@ -10,6 +10,8 @@ public class Platform_DefaultRenderer
 	public static final String __md_methods;
 	static {
 		__md_methods = 
+			"n_onTouchEvent:(Landroid/view/MotionEvent;)Z:GetOnTouchEvent_Landroid_view_MotionEvent_Handler\n" +
+			"n_dispatchTouchEvent:(Landroid/view/MotionEvent;)Z:GetDispatchTouchEvent_Landroid_view_MotionEvent_Handler\n" +
 			"";
 		mono.android.Runtime.register ("Xamarin.Forms.Platform.Android.Platform+DefaultRenderer, Xamarin.Forms.Platform.Android, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null", Platform_DefaultRenderer.class, __md_methods);
 	}
@@ -37,6 +39,22 @@ public class Platform_DefaultRenderer
 		if (getClass () == Platform_DefaultRenderer.class)
 			mono.android.TypeManager.Activate ("Xamarin.Forms.Platform.Android.Platform+DefaultRenderer, Xamarin.Forms.Platform.Android, Version=2.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
 	}
+
+
+	public boolean onTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_onTouchEvent (p0);
+	}
+
+	private native boolean n_onTouchEvent (android.view.MotionEvent p0);
+
+
+	public boolean dispatchTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_dispatchTouchEvent (p0);
+	}
+
+	private native boolean n_dispatchTouchEvent (android.view.MotionEvent p0);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
