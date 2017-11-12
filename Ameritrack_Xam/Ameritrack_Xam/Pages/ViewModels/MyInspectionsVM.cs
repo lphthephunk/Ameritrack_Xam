9191ddaf425a6ebe92d90d23abf91e28d66c6d9a
Justin Rose <justin@justncode.com>
2017-11-12 14:59:50 -0500

    Pull to refresh

--- Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/MyInspectionsVM.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/MyInspectionsVM.cs
@@ -3,21 +3,93 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Ameritrack_Xam.PCL.Interfaces;
 using Ameritrack_Xam.PCL.Models;
+using Ameritrack_Xam.PCL.Helpers;
 using Xamarin.Forms;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Ameritrack_Xam.Pages.ViewModels
 {
-    public class MyInspectionsVM
+    public class MyInspectionsVM : INotifyPropertyChanged
     {
         IDatabaseServices DatabaseService = DependencyService.Get<IDatabaseServices>();
+        public ObservableCollection<Report> ReportList { get; set; }
+        public static bool loaded = false;
+
+        private bool _isBusy = false;
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged("IsBusy");
+            }
+        }
+
+        public ICommand Refresh
+        {
+            get
+            {
+                return new Command(async () => 
+                {
+                    IsBusy = true;
+                    await GetReports();
+                    IsBusy = false;
+                });
+            }
+        }
 
         public MyInspectionsVM()
         {
+            ReportList = new ObservableCollection<Report>();
         }
 
-        public async Task<List<Report>> GetReportsByEmployee(Employee employee)
+        private async Task<ObservableCollection<Report>> GetReportsByEmployee(Employee employee)
         {
-            return await DatabaseService.GetReportsByEmployee(employee);
+            var reports = new ObservableCollection<Report>();
+
+            try 
+            {
+                
+                List<Report> list = await DatabaseService.GetReportsByEmployee(employee);
+                foreach (var report in list)
+                    reports.Add(report);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Couldn't retrieve reports for employee " + ex.Message);
+            }
+
+            return reports;
         }
+
+        public async Task GetReports()
+        {
+            ReportList.Clear();
+            var reports = await GetReportsByEmployee(UserDataCache.CurrentEmployeeData);
+
+            foreach (var report in reports)
+            {
+                ReportList.Add(report);
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed(this, new PropertyChangedEventArgs(name));
+
+        }
+
+        #endregion
     }
 }
