0fdb8d26cb4e477b622c746691da84a5ef90eae9
khoapham3 <kpham3@students.kennesaw.edu>
2017-11-16 13:25:20 -0500

    Added FaultList to ReportVM

--- Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/ReportVM.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/ReportVM.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Ameritrack_Xam.PCL.Interfaces;
+using Ameritrack_Xam.PCL.Models;
+using Xamarin.Forms;
+
 namespace Ameritrack_Xam.Pages.ViewModels
 {
     public class ReportVM
     {
+        IDatabaseServices DatabaseService = DependencyService.Get<IDatabaseServices>();
+        public ObservableCollection<Fault> FaultList { get; set; }
+
         public ReportVM()
         {
         }
