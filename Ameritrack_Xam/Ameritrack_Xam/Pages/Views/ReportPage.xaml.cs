8d9517bf601742c06bec41a1e8ab8c94ffb2fac9
khoapham3 <kpham3@students.kennesaw.edu>
2017-11-16 13:23:14 -0500

    Send MyInspections selected item to ReportPage

--- Ameritrack_Xam/Ameritrack_Xam/Pages/Views/ReportPage.xaml.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/Views/ReportPage.xaml.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Ameritrack_Xam.PCL.Models;
 using Xamarin.Forms;
 
 namespace Ameritrack_Xam.Pages.Views
 {
     public partial class ReportPage : ContentPage
     {
-        public ReportPage()
+        public ReportPage(Report report)
         {
             InitializeComponent();
         }
