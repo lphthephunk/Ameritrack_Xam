409f0a06eca4d3f7770de7a856073286f068ddf5
Justin Rose <justin@justncode.com>
2017-11-16 13:17:56 -0500

    Capture correct report

--- Ameritrack_Xam/Ameritrack_Xam/Pages/Views/MyInspectionsPage.xaml.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/Views/MyInspectionsPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System; using System.Collections.Generic; using Ameritrack_Xam.Pages.ViewModels; using System.Threading.Tasks; using Xamarin.Forms; using System.Collections.ObjectModel;   namespace Ameritrack_Xam.Pages.Views {     public partial class MyInspectionsPage : ContentPage     {         private MyInspectionsVM ViewModel;          public MyInspectionsPage()         {             InitializeComponent();              ViewModel = new MyInspectionsVM();             BindingContext = ViewModel;         }                   protected async override void OnAppearing()
+﻿using System; using System.Collections.Generic; using Ameritrack_Xam.Pages.ViewModels; using System.Threading.Tasks; using Xamarin.Forms; using System.Collections.ObjectModel; using Ameritrack_Xam.PCL.Models;   namespace Ameritrack_Xam.Pages.Views {     public partial class MyInspectionsPage : ContentPage     {         private MyInspectionsVM ViewModel;          public MyInspectionsPage()         {             InitializeComponent();              ViewModel = new MyInspectionsVM();             BindingContext = ViewModel;         }                   protected async override void OnAppearing()
         {             await ViewModel.GetReports(); 
             base.OnAppearing();
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {             var list = (ListView)sender;             var report = (Report)list.SelectedItem;
+            DisplayAlert("Captured Report", report.ClientName, "OK");
         }     } }
