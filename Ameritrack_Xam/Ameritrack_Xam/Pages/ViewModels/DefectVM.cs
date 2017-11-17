e76644b9ef4ac4c6cff6d27d039fbbece22b0e91
khoapham3 <kpham3@students.kennesaw.edu>
2017-11-17 00:15:45 -0500

    Added defectpage implementation

--- Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/DefectVM.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/ViewModels/DefectVM.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Ameritrack_Xam.PCL.Models;
+
 namespace Ameritrack_Xam.Pages
 {
     public class DefectVM
     {
-        public DefectVM()
+        private ObservableCollection<Fault> _listOfFaults { get; set; }
+        public ObservableCollection<Fault> ListOfFaults
         {
+            get { return _listOfFaults; }
+            set
+            {
+                _listOfFaults = value;
+                OnPropertyChanged(nameof(ListOfFaults));
+            }
         }
+
+        public DefectVM(ObservableCollection<Fault> faults)
+        {
+            ListOfFaults = faults;
+        }
+
+        public void GetDefects(ObservableCollection<Fault> faults)
+        {
+            // clear list here
+
+            ListOfFaults = faults;
+        }
+
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed(this, new PropertyChangedEventArgs(name));
+
+        }
+
+        #endregion
     }
 }
--- Ameritrack_Xam/Ameritrack_Xam/Pages/Views/DefectPage.xaml.cs
+++ Ameritrack_Xam/Ameritrack_Xam/Pages/Views/DefectPage.xaml.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using Ameritrack_Xam.PCL.Models;
 using Xamarin.Forms;
 
 namespace Ameritrack_Xam.Pages.Views
 {
     public partial class DefectPage : ContentPage
     {
-        public DefectPage()
+        DefectVM ViewModel;
+        ObservableCollection<Fault> faultList;
+
+        public DefectPage(ObservableCollection<Fault> faults)
         {
             InitializeComponent();
+
+            faultList = faults;
+            ViewModel = new DefectVM(faults);
+
+            BindingContext = ViewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            
+            base.OnAppearing();
         }
     }
 }
